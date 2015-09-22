42b9623d256496b8ce613bb9c53052a97844bf8d
Matthew Boston <matthew.boston@gmail.com>
2015-09-22 16:38:04 -0600

    shorter code without using a map

--- src/conway/gui.cljs
+++ src/conway/gui.cljs
@@ -1,8 +1,9 @@
 (ns conway.gui
   (:require [goog.object :as object]))
 
-(defonce cell-dimensions {:width 5 :height 5
-                          :spacing 7})
+(defonce width 5)
+(defonce height 5)
+(defonce spacing 7)
 
 (defn draw [diffs]
   (let [tmp_canvas (.createElement js/document "canvas")
@@ -13,13 +14,11 @@
     (object/set tmp_ctx "fillStyle" "#FAFAFA")
     (doall (map (fn [diff]
                   (.fillRect tmp_ctx
-                             (* (:x diff) (:spacing cell-dimensions)) (* (:y diff) (:spacing cell-dimensions))
-                             (:width cell-dimensions) (:height cell-dimensions)))
+                             (* (:x diff) spacing) (* (:y diff) spacing) width height))
                 (filter (fn [diff] (= :dead (:state diff))) diffs)))
     (object/set tmp_ctx "fillStyle" "#008CBA")
     (doall (map (fn [diff]
                   (.fillRect tmp_ctx
-                             (* (:x diff) (:spacing cell-dimensions)) (* (:y diff) (:spacing cell-dimensions))
-                             (:width cell-dimensions) (:height cell-dimensions)))
+                             (* (:x diff) spacing) (* (:y diff) spacing) width height))
                 (filter (fn [d] (= :live (:state d))) diffs)))
     (.drawImage (.getContext canvas "2d") tmp_canvas 0 0)))
