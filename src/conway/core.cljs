e794b367ab8e66c4107f3fa84c02abc42d4f9364
Matthew Boston <matthew.boston@gmail.com>
2015-09-22 11:42:44 -0600

    one generation

--- src/conway/core.cljs
+++ src/conway/core.cljs
@@ -1,4 +1,5 @@
-(ns conway.core)
+(ns conway.core
+  (:require [goog.object :as object]))
 
 (def columns 100)
 (def rows 100)
@@ -6,33 +7,80 @@
 (defonce cell-dimensions {:width 5 :height 5
                           :spacing 7})
 
-(defn generate [ctx]
-  (dotimes [x columns]
-    (dotimes [y rows])))
-
 (defn draw [ctx world]
   (dotimes [x columns]
     (dotimes [y rows]
       (if (= :dead (get-in world [y x]))
-        (aset ctx "fillStyle" "#eee")
-        (aset ctx "fillStyle" "#c00"))
+        (object/set ctx "fillStyle" "#eee")
+        (object/set ctx "fillStyle" "#c00"))
       (.fillRect ctx
                  (* x (:spacing cell-dimensions)) (* y (:spacing cell-dimensions))
                  (:width cell-dimensions) (:height cell-dimensions)))))
 
-(def seed-world
+(defn build-world [world]
   (into [] (map (partial into [])
-                (partition columns
-                           (for [x (range (inc columns))
-                                 y (range (inc rows))]
-                             (if (zero? (rand-int 10))
-                               :alive
-                               :dead))))))
+                (partition columns world))))
+
+(defn n [world x y] (get-in world [(dec y) x]))
+(defn ne [world x y] (get-in world [(dec y) (inc x)]))
+(defn e [world x y] (get-in world [y (inc x)]))
+(defn se [world x y] (get-in world [(inc y) (inc x)]))
+(defn s [world x y] (get-in world [(inc y) x]))
+(defn sw [world x y] (get-in world [(inc y) (dec x)]))
+(defn w [world x y] (get-in world [y (dec x)]))
+(defn nw [world x y] (get-in world [(dec y) (dec x)]))
+
+(defn live-neighbors [world x y]
+  (count (keep #{:live} ((juxt n ne e se s sw w nw) world x y))))
+
+(defn under-populated? [world x y]
+  (let [cell (get-in world [y x])]
+    (and (= :live cell)
+         (< (live-neighbors world x y) 2))))
+
+(defn lives-on? [world x y]
+  (let [cell (get-in world [y x])]
+    (and (= :live cell)
+         (or (= (live-neighbors world x y) 2)
+             (= (live-neighbors world x y) 3)))))
+
+(defn over-crowded? [world x y]
+  (let [cell (get-in world [y x])]
+    (and (= :live cell)
+         (> (live-neighbors world x y) 3))))
+
+(defn just-right? [world x y]
+  (let [cell (get-in world [y x])]
+    (and (= :dead cell)
+         (= (live-neighbors world x y) 3))))
+
+(defonce seed-world
+  (build-world
+   (for [x (range columns)
+         y (range rows)]
+     (if (zero? (rand-int 10))
+       :live
+       :dead))))
+
+(defn generate [ctx world]
+  (js/setTimeout
+   #(let [new-world (build-world
+                     (for [y (range rows)
+                           x (range columns)]
+                       (cond
+                         (under-populated? world x y) :dead
+                         (lives-on? world x y) :live
+                         (over-crowded? world x y) :dead
+                         (just-right? world x y) :live
+                         :else :dead)))]
+      (draw ctx new-world))
+   1000))
 
 (defn ^:export main []
   (let [canvas (.getElementById js/document "conway")
         ctx (.getContext canvas "2d")]
     (doto ctx
-      (draw seed-world))))
+      (draw seed-world)
+      (generate seed-world))))
 
 (main)
