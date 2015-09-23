7691c93f15462eac526a29f1af40faae1bc2ab15
Matthew Boston <matthew.boston@gmail.com>
2015-09-22 22:44:07 -0600

    improve performance of draw

--- src/conway/gui.cljs
+++ src/conway/gui.cljs
@@ -1,24 +1,16 @@
-(ns conway.gui
-  (:require [goog.object :as object]))
+(ns conway.gui)
 
 (defonce width 5)
 (defonce height 5)
 (defonce spacing 7)
 
+(defonce canvas (.getElementById js/document "conway"))
+(defonce ctx (.getContext canvas "2d"))
+
 (defn draw [diffs]
-  (let [tmp_canvas (.createElement js/document "canvas")
-        tmp_ctx (.getContext tmp_canvas "2d")
-        canvas (.getElementById js/document "conway")]
-    (object/set tmp_canvas "width" (object/get canvas "width"))
-    (object/set tmp_canvas "height" (object/get canvas "height"))
-    (object/set tmp_ctx "fillStyle" "#FAFAFA")
-    (doall (map (fn [diff]
-                  (.fillRect tmp_ctx
-                             (* (:x diff) spacing) (* (:y diff) spacing) width height))
-                (filter (fn [diff] (= :dead (:state diff))) diffs)))
-    (object/set tmp_ctx "fillStyle" "#008CBA")
-    (doall (map (fn [diff]
-                  (.fillRect tmp_ctx
-                             (* (:x diff) spacing) (* (:y diff) spacing) width height))
-                (filter (fn [d] (= :live (:state d))) diffs)))
-    (.drawImage (.getContext canvas "2d") tmp_canvas 0 0)))
+  (doall (for [diff diffs]
+           (do (if (= :dead (:state diff))
+                 (set! (.-fillStyle ctx) "#FAFAFA")
+                 (set! (.-fillStyle ctx) "#008CBA"))
+               (.fillRect ctx
+                          (* (:x diff) spacing) (* (:y diff) spacing) width height)))))
