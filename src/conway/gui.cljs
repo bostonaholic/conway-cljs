1435c327ed1aa5d994835e1f80edf1c079fca794
Matthew Boston <matthew.boston@gmail.com>
2015-09-23 18:51:58 -0600

    bring some colors into the game

--- src/conway/gui.cljs
+++ src/conway/gui.cljs
@@ -10,6 +10,6 @@
   (doall (for [diff diffs]
            (do (if (= :dead (:state diff))
                  (set! (.-fillStyle ctx) "#FAFAFA")
-                 (set! (.-fillStyle ctx) "#008CBA"))
+                 (set! (.-fillStyle ctx) (rand-nth ["#0A35C7" "#0A6CD1" "#008CBA" "#0AD1CE" "#0AC78E"])))
                (.fillRect ctx
                           (* (:x diff) cell-width) (* (:y diff) cell-height) 5 5)))))
