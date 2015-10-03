592399e2afdb0cc81ec0216cda922dd206ba9b9a
Matthew Boston <matthew.boston@gmail.com>
2015-10-02 21:11:01 -0600

    refactor gui

--- src/conway/gui.cljs
+++ src/conway/gui.cljs
@@ -3,13 +3,28 @@
 (defonce canvas (.getElementById js/document "conway"))
 (defonce ctx (.getContext canvas "2d"))
 
-(defonce cell-width 7)
-(defonce cell-height 7)
+(defonce cell-width 5)
+(defonce cell-height 5)
+(defonce cell-border 2)
+
+(defn set-color [context color]
+  (set! (.-fillStyle context) color))
+
+(defn draw-cell [context color x y]
+  (doto context
+    (set-color color)
+    (.fillRect x y cell-width cell-height)))
+
+(defn draw-dead-cell [context x y]
+  (draw-cell context "#FAFAFA" x y))
+
+(defn draw-live-cell [context x y]
+  (draw-cell context (rand-nth ["#0A35C7" "#0A6CD1" "#008CBA" "#0AD1CE" "#0AC78E"]) x y))
+
+(defn draw-diff [diff]
+  (if (= :dead (:state diff))
+    (draw-dead-cell ctx (* (:x diff) (+ cell-width cell-border)) (* (:y diff) (+ cell-height cell-border)))
+    (draw-live-cell ctx (* (:x diff) (+ cell-width cell-border)) (* (:y diff) (+ cell-height cell-border)))))
 
 (defn draw [diffs]
-  (doall (for [diff diffs]
-           (do (if (= :dead (:state diff))
-                 (set! (.-fillStyle ctx) "#FAFAFA")
-                 (set! (.-fillStyle ctx) (rand-nth ["#0A35C7" "#0A6CD1" "#008CBA" "#0AD1CE" "#0AC78E"])))
-               (.fillRect ctx
-                          (* (:x diff) cell-width) (* (:y diff) cell-height) 5 5)))))
+  (doall (map draw-diff diffs)))
