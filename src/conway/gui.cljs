f2a9a24bdd14f3d5d53109e3cb6bea08fa4ad84f
Matthew Boston <matthew@matthewboston.com>
2019-12-09 21:09:40 -0700

    mark private functions in gui

--- src/conway/gui.cljs
+++ src/conway/gui.cljs
@@ -1,40 +1,40 @@
 (ns conway.gui)
 
-(defn get-canvas []
+(defn- get-canvas []
   (.getElementById js/document "conway"))
 
-(defn create-canvas []
+(defn- create-canvas []
   (let [canvas (doto (.createElement js/document "canvas")
                  (aset "id" "conway"))]
     (when (.-body js/document)
       (.appendChild (.-body js/document) canvas))
     canvas))
 
-(defonce canvas (or (get-canvas)
-                    (create-canvas)))
+(defonce ^:private canvas (or (get-canvas)
+                              (create-canvas)))
 
 
-(defonce ctx (.getContext canvas "2d"))
+(defonce ^:private ctx (.getContext canvas "2d"))
 
-(defonce cell-width 5)
-(defonce cell-height 5)
-(defonce cell-border 2)
+(defonce ^:private cell-width 5)
+(defonce ^:private cell-height 5)
+(defonce ^:private cell-border 2)
 
-(defn set-color [context color]
+(defn- set-color [context color]
   (set! (.-fillStyle context) color))
 
-(defn draw-cell [context color x y]
+(defn- draw-cell [context color x y]
   (doto context
     (set-color color)
     (.fillRect x y cell-width cell-height)))
 
-(defn draw-dead-cell [context x y]
+(defn- draw-dead-cell [context x y]
   (draw-cell context "#FAFAFA" x y))
 
-(defn draw-live-cell [context x y]
+(defn- draw-live-cell [context x y]
   (draw-cell context (rand-nth ["#0A35C7" "#0A6CD1" "#008CBA" "#0AD1CE" "#0AC78E"]) x y))
 
-(defn draw-diff [diff]
+(defn- draw-diff [diff]
   (if (= :dead (:state diff))
     (draw-dead-cell ctx (* (:x diff) (+ cell-width cell-border)) (* (:y diff) (+ cell-height cell-border)))
     (draw-live-cell ctx (* (:x diff) (+ cell-width cell-border)) (* (:y diff) (+ cell-height cell-border)))))
